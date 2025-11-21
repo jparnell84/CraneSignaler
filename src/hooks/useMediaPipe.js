64ea1b1552fc149f2661e87649a0f183775c55e0
Jonathan Parnell <34899813+jparnell84@users.noreply.github.com>
2025-11-21 08:59:08 -0800

    Implement useMediaPipe hook for video processing

--- src/hooks/useMediaPipe.js
+++ src/hooks/useMediaPipe.js
@@ -1,1 +1,30 @@
+import { useEffect, useRef } from 'react';
+import { Holistic } from '@mediapipe/holistic';
+import { Camera } from '@mediapipe/camera_utils';
+
+export const useMediaPipe = (videoRef, onResults) => {
+    useEffect(() => {
+        const holistic = new Holistic({
+            locateFile: (file) => `https://cdn.jsdelivr.net/npm/@mediapipe/holistic/${file}`
+        });
+        
+        holistic.setOptions({
+            modelComplexity: 1,
+            smoothLandmarks: true,
+        });
+        
+        holistic.onResults(onResults);
+
+        if (videoRef.current && videoRef.current.video) {
+            const camera = new Camera(videoRef.current.video, {
+                onFrame: async () => {
+                    await holistic.send({image: videoRef.current.video});
+                },
+                width: 1280,
+                height: 720
+            });
+            camera.start();
+        }
+    }, []);
+};
 
